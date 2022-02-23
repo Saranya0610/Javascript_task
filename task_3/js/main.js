be4255e6b619e673553d5c0727f4d2db89148b72
Saranya0610 <93171922+Saranya0610@users.noreply.github.com>
2022-02-23 08:38:24 -0800

    Create main.js

---
+++ task_3/js/main.js
@@ -0,0 +1,7 @@
+var text="What is your favorite flavor of ice cream?" ;
+
+var  myArray = text.split(" ") ;
+var position =text.search("favorite") ;
+  
+document.write( " " + myArray +  "<br>") ;
+document.write(position) ;

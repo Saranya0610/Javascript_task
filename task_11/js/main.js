eecd7ac47a454b19ae23c48b0d713d296458d85d
Saranya0610 <93171922+Saranya0610@users.noreply.github.com>
2022-02-23 08:56:16 -0800

    Create main.js

---
+++ task_11/js/main.js
@@ -0,0 +1,11 @@
+var numArray = [45, 78, 99, 65, 23, 25, 84, 2];
+num = numArray.sort();
+document.write("Acending Order  : </br>"  + num + "");
+
+
+
+ document.write("</br>");
+var color =  ["Blue ", "Green", "Red", "Orange", "Violet", "Violet", "Violet", "Indigo", "Blue ", "Blue ", "Blue ",  "Yellow "] ;
+var uniqueColor = [...new Set(color)];
+document.write("Removed Duplicate : </br> " + uniqueColor + "");
+    

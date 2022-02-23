bc2c307423f84b8d743e19bf98f09d173d2977ae
Saranya0610 <93171922+Saranya0610@users.noreply.github.com>
2022-02-23 08:37:00 -0800

    Create main.js

---
+++ task_2/js/main.js
@@ -0,0 +1,11 @@
+var Name1 = "Saranya" ;
+var Name2 =  "Ganesan" ;
+var combine = Name1+Name2 ;
+var length = combine.length ;
+
+
+document.write(" Length Of The String : " + length + "") ;
+document.write("<br>") ;
+document.write(" Contatination  Of The String : " + combine + "") ; 
+
+

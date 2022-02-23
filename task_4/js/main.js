2e0832176540db4f067436187e53ec4f4a3b66d7
Saranya0610 <93171922+Saranya0610@users.noreply.github.com>
2022-02-23 08:39:58 -0800

    Create main.js

---
+++ task_4/js/main.js
@@ -0,0 +1,53 @@
+//charAT()
+
+var str="When a JavaScript variable is declared with the keyword 'new', the variable is created as an object";  
+document.write("String Method 1: charAt() = " + str.charAt(20));  
+
+
+document.write("<br>") ;
+document.write("<br>") ;
+// concat()
+
+var str1 = "Saranya ";  
+
+var str2 = "Ganesan";  
+
+var str3 = str1+str2;  
+
+document.write("String Method 2: concat() = " +  str3);
+
+
+
+document.write("<br>") ;
+document.write("<br>") ;
+
+
+
+// indexOf()
+
+var str1 = "When a JavaScript variable is declared with the keyword 'new', the variable is created as an object";  
+
+var result = str1.indexOf("with");  
+
+document.write("String Method 3: indexOf() = " + result); 
+
+document.write("<br>") ;
+document.write("<br>") ;
+// endsWith()
+
+var str1 = "When a JavaScript variable is declared with the keyword 'new', the variable is created as an object";  
+
+var result = str1.endsWith("ct");
+document.write("String Method 4: endsWith() = " + result); 
+
+document.write("<br>") ;
+document.write("<br>") ;
+
+//fromCharCode()
+var str1 = "When a JavaScript variable is declared with the keyword 'new', the variable is created as an object";  
+
+var result = str1.fromCharCode(97);
+document.write("String Method 5: fromCharCode() = " + result); 
+
+document.write("<br>") ;
+document.write("<br>") ;

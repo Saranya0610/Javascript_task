3f6bf9d00106be397fac0e263ff169cae56362f5
Saranya0610 <93171922+Saranya0610@users.noreply.github.com>
2022-02-23 08:53:24 -0800

    Create main.js

---
+++ task_10/js/main.js
@@ -0,0 +1,13 @@
+
+var evenArray = [];
+for (var i=0; i <= 15; i+=2) {
+    evenArray.push(i);
+    document.write(evenArray[i]);
+
+}
+document.write("<br>");
+ var oddAray = [];
+for (var i=1; i <=15; i+=2) {
+    oddAray.push(i);
+    document.write(oddAray[i]);
+}

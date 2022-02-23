c7009eb0a86b56f262eace63ccbc8cdbc4eb69c7
Saranya0610 <93171922+Saranya0610@users.noreply.github.com>
2022-02-23 09:02:44 -0800

    Create main.js

---
+++ task_14/js/main.js
@@ -0,0 +1,11 @@
+var today = new Date();
+var date = today.getDate(); 
+document.write("TodayDate </br>" + date + "</br>"); 
+
+var today = new Date();
+var month = today.getMonth();
+document.write("CurrentMonth </br>" + month + "</br>"); 
+
+var today = new Date();
+var year = today.getFullYear();
+document.write("CurrentYear </br>" + year);
